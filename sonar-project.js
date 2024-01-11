8d69d4f5b138f565054e079a2982887ce53a4f76
Psuchitra2424 <151764486+suchitra2424@users.noreply.github.com>
2024-01-11 17:38:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.252.163.227:9000/',
+  serverUrl: 'http://172.31.7.111:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_98678b1f694ecef9c30c5dd3d2871211833ba8ee',
+	    'sonar.login': 'squ_64b90e913b1f10a318764e9d553cca7e7fc9eeca',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
